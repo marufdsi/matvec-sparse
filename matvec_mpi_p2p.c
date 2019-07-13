93d0843dfb5d0d61c39371c73c3e646ba6e4518f
marufdsi <maruf.csea1@gmail.com>
2019-07-13 18:17:10 -0400

    Total Communication calculation addded

--- matvec_mpi_p2p.c
+++ matvec_mpi_p2p.c
@@ -307,12 +307,12 @@ int main(int argc, char *argv[]) {
     }*/
     /* Matrix-vector multiplication for each processes */
     res = mat_vec_mult_parallel(rank, nprocs, buf_i_idx, buf_j_idx, buf_values, buf_x, row_count, row_offset);
-    if (rank == MASTER) {
+    /*if (rank == MASTER) {
         printf("Result Y= ");
         for (int i = 0; i < proc_info[MASTER].N; ++i) {
             printf("|%lf| ", res[i]);
         }
-    }
+    }*/
 
 
     /*MPI_Barrier(MPI_COMM_WORLD);
