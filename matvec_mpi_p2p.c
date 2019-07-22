04f84137912cdf993268eb28a23faaf775146cda
maruf <maruf.csea1@gmail.com>
2019-07-21 21:36:13 -0400

    Removed Partioning

--- matvec_mpi_p2p.c
+++ matvec_mpi_p2p.c
@@ -90,8 +90,10 @@ double *mat_vec_mult_parallel(int rank, int nprocs, int *buf_i_idx, int *buf_j_i
     /// Wait until send request delivered to through network.
     MPI_Waitall(nprocs, send_reqs, MPI_STATUS_IGNORE);
     free(vecFromRemotePros);
-    MPI_Request_free(send_reqs);
-    MPI_Request_free(recv_reqs);
+    for (int p = 0; p < nprocs; ++p) {
+        MPI_Request_free(send_reqs[p]);
+        MPI_Request_free(recv_reqs[p]);
+    }
     return y;
 }
 
