49ba2df0afcb39899012211c57835b721ab17133
maruf <maruf.csea1@gmail.com>
2019-07-26 18:08:14 -0400

    Total communication calculated

--- csr_mpi_model.c
+++ csr_mpi_model.c
@@ -297,6 +297,12 @@ int main(int argc, char *argv[]) {
     row_ptr = (int *) malloc((mat_row + 1) * sizeof(int));
     col_ptr = (int *) malloc(procs_info[rank].NZ * sizeof(int));
     val_ptr = (double *) malloc(procs_info[rank].NZ * sizeof(double));
+
+    testMap(rank);
+    printf("[%d] Done matrix creation\n", rank);
+    MPI_Finalize();
+    return 0;
+
     /// Create random CSR matrix with the given parameter
     if (csr_random_mat(rank, procs_info, row_ptr, col_ptr, val_ptr, mat_row, mat_col, nonZeroPerRow) != 1) {
         printf("[%d] Matrix Creation Failed process=%d, matrix size=%d, nonzero=%d\n", rank, nRanks, (procs_info[rank].M*nRanks),
@@ -311,9 +317,6 @@ int main(int argc, char *argv[]) {
             printf("\n");
         }
 //    }
-    printf("[%d] Done matrix creation\n", rank);
-    MPI_Finalize();
-    return 0;
     /// Create vector x and fill with 1.0
     buf_x = (double *) malloc_or_exit(mat_row * sizeof(double));
     for (int i = 0; i < mat_row; i++) {
