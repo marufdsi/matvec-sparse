a38ef51dd63b4e476e6254ea797b8f1b577f761a
maruf <maruf.csea1@gmail.com>
2019-08-21 19:12:22 -0400

    SpMV Model updated

---
+++ batchRun.sh
@@ -0,0 +1,13 @@
+sbatch mpiJob.sh input/nlpkkt160_128.mtx
+sbatch mpiJob.sh input/nlpkkt240_128.mtx
+sbatch mpiJob.sh input/cage15_128.mtx
+sbatch mpiJob.sh input/G3_circuit_128.mtx
+sbatch mpiJob.sh input/kkt_power_128.mtx
+sbatch mpiJob.sh input/ecology1_128.mtx
+
+sbatch mpiJob.sh input/uk-2002_128.mtx
+sbatch mpiJob.sh input/asia_128.mtx
+sbatch mpiJob.sh input/NLR_128.mtx
+sbatch mpiJob.sh input/coPapersDBLP_128.mtx
+sbatch mpiJob.sh input/coPapersCiteseer_128.mtx
+
