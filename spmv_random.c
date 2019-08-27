aee1173051633d6f488bc0eb5cc286df06c07576
maruf <maruf.csea1@gmail.com>
2019-08-27 18:27:14 -0400

    SpMV Model updated

--- spmv_random.c
+++ spmv_random.c
@@ -109,18 +109,17 @@ int main(int argc, char *argv[]) {
 
     /// Share process info among all the processes
     MPI_Allgather(&ranks_info[rank], 1, procs_info_type, procs_info, 1, procs_info_type, MPI_COMM_WORLD);
-
+/*
     MPI_Bcast (x, ranks_info[rank].M, MPI_FLOAT, col_rank, commcol);
 
     MPI_Barrier(MPI_COMM_WORLD);
     printf("[%d] Reading Matrix done!\n", rank);
     MPI_Finalize();
-    return 0;
-    MPI_Barrier(MPI_COMM_WORLD);
+    return 0;*/
 //    printf("[%d] Reading Matrix and broad casting done!\n", rank);
     /// Start sparse matrix vector multiplication for each rank
-    double start_time = MPI_Wtime();
     MPI_Barrier(MPI_COMM_WORLD);
+    double start_time = MPI_Wtime();
     for (int r = 0; r < total_run; ++r) {
         //broadcast X along column communicator
         MPI_Bcast (x, ranks_info[rank].M, MPI_FLOAT, col_rank, commcol); //col_rank is the one with the correct information
