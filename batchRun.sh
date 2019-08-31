ec6b4862299ac1ffeb3293e00a99aa065bb87287
Md Maruf Hossain <maruf.csea1@gmail.com>
2019-08-30 20:23:36 -0500

    resultset updated

--- batchRun.sh
+++ batchRun.sh
@@ -1,13 +1,20 @@
-sbatch mpiJob.sh input/nlpkkt160_128.mtx
-sbatch mpiJob.sh input/nlpkkt240_128.mtx
-sbatch mpiJob.sh input/cage15_128.mtx
-sbatch mpiJob.sh input/G3_circuit_128.mtx
-sbatch mpiJob.sh input/kkt_power_128.mtx
-sbatch mpiJob.sh input/ecology1_128.mtx
+sbatch mpiJob.sh input/nlpkkt160_900.mtx
+sbatch mpiJob.sh input/nlpkkt240_900.mtx
+sbatch mpiJob.sh input/cage15_900.mtx
+sbatch mpiJob.sh input/G3_circuit_900.mtx
+sbatch mpiJob.sh input/kkt_power_900.mtx
+sbatch mpiJob.sh input/ecology1_900.mtx
 
-sbatch mpiJob.sh input/uk-2002_128.mtx
-sbatch mpiJob.sh input/asia_128.mtx
-sbatch mpiJob.sh input/NLR_128.mtx
-sbatch mpiJob.sh input/coPapersDBLP_128.mtx
-sbatch mpiJob.sh input/coPapersCiteseer_128.mtx
-
+sbatch mpiJob.sh input/uk-2002_900.mtx
+sbatch mpiJob.sh input/asia_900.mtx
+sbatch mpiJob.sh input/NLR_900.mtx
+sbatch mpiJob.sh input/coPapersDBLP_900.mtx
+sbatch mpiJob.sh input/coPapersCiteseer_900.mtx
+sbatch mpiJob.sh input/copter2_900.mtx
+sbatch mpiJob.sh input/333SP_900.mtx
+sbatch mpiJob.sh input/af_shell10_900.mtx
+sbatch mpiJob.sh input/AS365_900.mtx
+sbatch mpiJob.sh input/europe_900.mtx
+sbatch mpiJob.sh input/M6_900.mtx
+sbatch mpiJob.sh input/nlpkkt200_900.mtx
+sbatch mpiJob.sh input/thermal2_900.mtx
