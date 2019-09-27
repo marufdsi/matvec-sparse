c8ea225df77ef5821e033c56db06f668ae79a031
marufdsi <maruf.csea1@gmail.com>
2019-09-26 21:44:23 -0400

    Matrix creation method added

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ LDFLAGS = -lm -lrt
 # Object files from libraries
 OBJ = mmio.o mmio-wrapper.o partition.o util.o
 
-all: matvec_seq matvec_mpi_p2p matvec_mpi_bcast matvec_mpi_calculation csr_mpi_spmv csr_mpi_model csr_random_spmv_model csr_spmv csr_mpi_reduced_spmv spmv_p2p comm_p2p mult_p2p spmv_random read_file spmv_random_model omp_spmv_model omp_spmv
+all: matvec_seq matvec_mpi_p2p matvec_mpi_bcast matvec_mpi_calculation csr_mpi_spmv csr_mpi_model csr_random_spmv_model csr_spmv csr_mpi_reduced_spmv spmv_p2p comm_p2p mult_p2p spmv_random read_file spmv_random_model omp_spmv_model omp_spmv create_matrix
 
 matvec_seq: matvec_seq.c $(OBJ) stopwatch.o
 	$(CC) $(CFLAGS) $(OBJ) stopwatch.o $< -o $@ $(LDFLAGS)
@@ -72,9 +72,12 @@ omp_spmv_model: omp_spmv_model.c $(OBJ)
 omp_spmv: omp_spmv.c $(OBJ)
 	$(CC) $(CFLAGS) $(OMP_FLAGS) $(OBJ) $< -o $@ $(LDFLAGS)
 
+create_matrix: create_matrix.c $(OBJ)
+	$(CC) $(CFLAGS) $(OMP_FLAGS) $(OBJ) $< -o $@ $(LDFLAGS)
+
 %.o: %.c %.h
 	$(CC) $(CFLAGS) -c $<
 
 .PHONY: clean
 clean:
-	rm -f matvec_seq matvec_mpi_bcast matvec_mpi_p2p matvec_mpi_calculation csr_mpi_spmv csr_mpi_model csr_random_spmv_model csr_spmv csr_mpi_reduced_spmv spmv_p2p comm_p2p mult_p2p spmv_random read_file spmv_random_model omp_spmv_model omp_spmv *.o
+	rm -f matvec_seq matvec_mpi_bcast matvec_mpi_p2p matvec_mpi_calculation csr_mpi_spmv csr_mpi_model csr_random_spmv_model csr_spmv csr_mpi_reduced_spmv spmv_p2p comm_p2p mult_p2p spmv_random read_file spmv_random_model omp_spmv_model omp_spmv create_matrix *.o
---
+++ create_matrix.c
@@ -0,0 +1,73 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mmio-wrapper.h"
+#include "util.h"
+#include "partition.h"
+
+
+void  create_random_matrix(int m, int n, int nnz_per_row, char *out_file){
+    FILE *newMat;
+    if (!(newMat = fopen(strcat(out_file, ".mtx"), "w"))) {
+        printf("fopen: failed to open file '%s'", out_file);
+        return;
+    }
+    fprintf(newMat, "%%%MatrixMarket matrix coordinate real general\n");
+    fprintf(newMat, "%d %d %d\n", m, n, m*nnz_per_row);
+    srand(time(NULL) * (n%m + 1));
+    printf("*********** Initialization DOne **********");
+    int *trackIndex = (int *) malloc_or_exit( n * sizeof(int));
+    for (int i = 0; i < m; ++i) {
+        memset(trackIndex, -1, n * sizeof(int));
+        for (int j = 0; j < nnz_per_row; ++j) {
+            int randColIdx;
+            do {
+                randColIdx = rand() % n;
+            } while (trackIndex[randColIdx] != -1);
+            trackIndex[ randColIdx] = 1.0;
+            fprintf(newMat, "%d %d %lf\n", i, randColIdx, (randColIdx%10 +1));
+        }
+    }
+    free(trackIndex);
+/// close file
+    if (fclose(newMat) != 0) {
+        fprintf(stderr, "fopen: failed to open file '%s'", out_file);
+        return;
+    }
+}
+
+void  create_diagonal_matrix(){
+
+}
+
+/**
+ *
+ * @param argc
+ * @param argv
+ * @return
+ * Main method of the SpMV model
+ * This model will able to predict the run time of the sparse matrix
+ */
+int main(int argc, char *argv[]) {
+
+    char *out_file;
+    int m, n, nnz_per_row;
+    if (argc < 4) {
+        printf("Usage: %s m n nnz_per_row output_file]\n", argv[0]);
+        return 0;
+    } else {
+        m = atoi(argv[1]);
+        n = atoi(argv[2]);
+        nnz_per_row = atoi(argv[3]);
+        out_file = argv[4];
+    }
+
+    create_random_matrix(m, n, nnz_per_row, out_file);
+
+    return 0;
+}
+
