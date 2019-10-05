1aff9a3bdae462394382f432b8c2b87db3ea1caf
marufdsi <maruf.csea1@gmail.com>
2019-10-04 23:38:30 -0400

    Reorder code adde

--- mmio-wrapper.c
+++ mmio-wrapper.c
@@ -84,7 +84,8 @@ int rank_wise_read_matrix(const char *filename, int **i_idx, int **j_idx, double
 
     /* open the file */
     char rank_wise_filename[MM_MAX_LINE_LENGTH];
-    char *_ptr = strtok(filename, ".");
+    char *file_n = filename;
+    char *_ptr = strtok(file_n, ".");
     sprintf(rank_wise_filename, "%s_%d.%s", _ptr, rank, strtok(NULL, "-"));
     if ((f = fopen(rank_wise_filename, "r")) == NULL) {
         fprintf(stderr, "Cannot open '%s'\n", rank_wise_filename);
@@ -158,7 +159,8 @@ int test_csr_read_2D_partitioned_mat(const char *filename, int **row_ptr, int **
 
     /* open the file */
     char rank_wise_filename[MM_MAX_LINE_LENGTH];
-    char *_ptr = strtok(filename, ".");
+    char *file_n = filename;
+    char *_ptr = strtok(file_n, ".");
     sprintf(rank_wise_filename, "%s_%d.%s", _ptr, rank, strtok(NULL, "-"));
     if ((f = fopen(rank_wise_filename, "r")) == NULL) {
         printf("Cannot open '%s'\n", rank_wise_filename);
@@ -259,7 +261,8 @@ int csr_read_2D_partitioned_mat(const char *filename, int **row_ptr, int **col_p
 
     /* open the file */
     char rank_wise_filename[MM_MAX_LINE_LENGTH];
-    char *_ptr = strtok(filename, ".");
+    char *file_n = filename;
+    char *_ptr = strtok(file_n, ".");
     sprintf(rank_wise_filename, "%s_%d.%s", _ptr, rank, strtok(NULL, "-"));
     if ((f = fopen(rank_wise_filename, "r")) == NULL) {
         printf("Cannot open '%s'\n", rank_wise_filename);
@@ -355,7 +358,8 @@ int rank_wise_read_matrix_csr(const char *filename, int **row_ptr, int **col_ptr
 
     /* open the file */
     char rank_wise_filename[MM_MAX_LINE_LENGTH];
-    char *_ptr = strtok(filename, ".");
+    char *file_n = filename;
+    char *_ptr = strtok(file_n, ".");
     sprintf(rank_wise_filename, "%s_%d.%s", _ptr, rank, strtok(NULL, "-"));
     if ((f = fopen(rank_wise_filename, "r")) == NULL) {
         fprintf(stderr, "Cannot open '%s'\n", rank_wise_filename);
