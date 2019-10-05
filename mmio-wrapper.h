4cd62db34808a56e2ef2899454750a381b3dda32
marufdsi <maruf.csea1@gmail.com>
2019-10-04 23:35:48 -0400

    Reorder code adde

--- mmio-wrapper.h
+++ mmio-wrapper.h
@@ -10,10 +10,10 @@
 #include "util.h"
 
 int read_matrix (const char * filename, int **i_idx, int **j_idx, double **values, int *N, int *NZ);
-int rank_wise_read_matrix (char * filename, int **i_idx, int **j_idx, double **values, int *M, int *N, int *NZ, int *first_row, int *last_row, int rank);
-int rank_wise_read_matrix_csr (char * filename, int **row_ptr, int **col_ptr, double **val_ptr, proc_info_t **ranks_info, int rank, int *offDiagonalElements);
-int csr_read_2D_partitioned_mat(char * filename, int **row_ptr, int **col_ptr, double **val_ptr, proc_info_t **ranks_info, int sqrRank, int rank);
-int test_csr_read_2D_partitioned_mat(char *filename, int **row_ptr, int **col_ptr, double **val_ptr,
+int rank_wise_read_matrix (const char * filename, int **i_idx, int **j_idx, double **values, int *M, int *N, int *NZ, int *first_row, int *last_row, int rank);
+int rank_wise_read_matrix_csr (const char * filename, int **row_ptr, int **col_ptr, double **val_ptr, proc_info_t **ranks_info, int rank, int *offDiagonalElements);
+int csr_read_2D_partitioned_mat(const char * filename, int **row_ptr, int **col_ptr, double **val_ptr, proc_info_t **ranks_info, int sqrRank, int rank);
+int test_csr_read_2D_partitioned_mat(const char *filename, int **row_ptr, int **col_ptr, double **val_ptr,
                                      int sqrRank, int rank);
 int read_coo_matrix_to_csr(const char *filename, int **row_ptr, int **col_ptr, ValueType **val_ptr, int *mat_row, int *_nnz);
 int read_coo_matrix_to_csr_with_max_deg(const char *filename, int **row_ptr, int **col_ptr, ValueType **val_ptr, int *mat_row, int *_nnz, int *max_deg);
